2c60972828b650fbea2b8a6ea2ea877b9596571b
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-07-01 19:00:06 +0000

    trabajo completo

---
+++ src/js/icons.js
@@ -0,0 +1,6 @@
+import fontawesome from "@fontawesome/fontawesome";
+import faclock from "@fontawesome/fontawesome-free-regular/faClock";
+fontawesome.config = {
+    autoReplaceSvg: "nest"
+};
+fontawesome.library.add(faclock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,24 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function () {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	counter++;
+
+	ReactDOM.render(
+		<Home
+			digitoOne={one}
+			digitoTwo={two}
+			digitoThree={three}
+			digitoFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
